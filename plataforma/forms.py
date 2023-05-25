4f92a916a3cea2c09f17d42ad94b3f48cdc3ae5d
Yoshloaa <brayjosegon@gmail.com>
2023-05-25 11:22:33 -0600

    editado un documento

--- plataforma/forms.py
+++ plataforma/forms.py
@@ -10,7 +10,7 @@ class CreacionP(ModelForm):
     class Meta:
         model= pacientes1
         fields=['image','nombre', 'apellidoP', 'apellidoM','Fecha_na']
-class subirDoc(ModelForm):
-    class Meta:
-        model = documentos
+#class subirDoc(ModelForm):
+ #   class Meta:
+   #     model = documentos
         
